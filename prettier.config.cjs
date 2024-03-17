3297ee6247baead8fbaca44a889c10f73f1c13b0
josh900 <jm5522@sbcglobal.net>
2024-03-17 10:34:16 -0700

    Update prettier.config.cjs

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -33,4 +33,5 @@ module.exports = {
   importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
   importOrderMergeDuplicateImports: true,
   importOrderCombineTypeAndValueImports: true
+  
 }
