b66996be914e841136ea05f7d42070fc58d9127a
josh900 <jm5522@sbcglobal.net>
2024-03-18 09:18:07 -0700

    update

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "Skoop AI"
+const APP_DEFAULT_TITLE = "Skoop AI"
+const APP_TITLE_TEMPLATE = "%s - Skoop AI"
+const APP_DESCRIPTION = "Skoop AI Chat"
 
 interface RootLayoutProps {
   children: ReactNode
